9b13acf330e85812603054ea92b7c62f0674362c
Davi Gabriel <davianjogabriel@gmail.com>
2025-10-04 22:30:00 -0300

    aaaaa

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 import Home from './Home'
 import Product from './Product'
 
 export default function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:id" element={<Product />} />
       </Routes>
-    </Router>
+    </HashRouter>
   )
 }
