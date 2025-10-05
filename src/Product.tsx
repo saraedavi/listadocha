887c379d55266382bdc89252a059f25850af1dcb
Davi Gabriel <davianjogabriel@gmail.com>
2025-10-04 22:11:22 -0300

    aa

--- src/Product.tsx
+++ src/Product.tsx
@@ -1,4 +1,5 @@
 import { useParams, Link } from 'react-router-dom'
+import { useEffect } from 'react'
 import { Button, Card, Container, Footer, Price } from './styles'
 
 import allegra from '../src/assets/1.jpg'
@@ -15,97 +16,18 @@ import descascador from '../src/assets/descascador.jpg'
 import cafe from '../src/assets/cafe.jpg'
 
 const products = [
-  {
-    id: 1,
-    name: 'Arroz 5 kg Tupperware',
-    price: '99,90',
-    description: 'Descrição do Produto A',
-    image: arroz
-  },
-  {
-    id: 2,
-    name: 'Feijão 2 kg Tupperware',
-    price: '96,90',
-    description: 'Descrição do Produto A',
-    image: feijao
-  },
-  {
-    id: 3,
-    name: 'Farinha 1,8 kg Tupperware',
-    price: '96,90',
-    description: 'Descrição do Produto A',
-    image: farinha
-  },
-  {
-    id: 4,
-    name: 'Açúcar 1,4 kg Tupperware',
-    price: '83,90',
-    description: 'Descrição do Produto A',
-    image: acucar
-  },
-  {
-    id: 5,
-    name: 'Tigela Allegra 2,5 lt',
-    price: '139,90',
-    description: 'Descrição do Produto A',
-    image: allegra
-  },
-  {
-    id: 7,
-    name: 'Tigela Aactualite Tupperware 10 LT ',
-    price: '119,90',
-    description: 'Descrição do Produto A',
-    image: actualite
-  },
-  {
-    id: 8,
-    name: 'Centrífuga Plus Tupperware 3,9 LT',
-    price: '119,90',
-    description: 'Descrição do Produto A',
-    image: centrifuga
-  },
-  {
-    id: 9,
-    name: 'Café com Filtro Tupperware 3,7 LT',
-    price: '137,90',
-    description: 'Descrição do Produto A',
-    image: cafe
-  },
-  {
-    id: 10,
-    name: '4 Tigelinha Alegra 250 ml',
-    price: '79,60',
-    description: 'Descrição do Produto C',
-    image: tigela
-  },
-  {
-    id: 11,
-    name: 'Cristalwave Tupperware 1 lt',
-    price: '84,90',
-    description: 'Descrição do Produto C',
-    image: cristalwave1
-  },
-  {
-    id: 12,
-    name: 'Cristalwave Tupperware 3 lt',
-    price: '89,90',
-    description: 'Descrição do Produto C',
-    image: cristalwave2
-  },
-  {
-    id: 13,
-    name: 'Descascador Tupperware',
-    price: '34,90',
-    description: 'Descrição do Produto C',
-    image: descascador
-  },
-  {
-    id: 10,
-    name: '4 Tigelinha Alegra 250 ml',
-    price: '79,60',
-    description: 'Descrição do Produto C',
-    image: tigela
-  }
+  { id: 1, name: 'Arroz 5 kg Tupperware', price: '99,90', image: arroz },
+  { id: 2, name: 'Feijão 2 kg Tupperware', price: '96,90', image: feijao },
+  { id: 3, name: 'Farinha 1,8 kg Tupperware', price: '96,90', image: farinha },
+  { id: 4, name: 'Açúcar 1,4 kg Tupperware', price: '83,90', image: acucar },
+  { id: 5, name: 'Tigela Allegra 2,5 lt', price: '139,90', image: allegra },
+  { id: 7, name: 'Tigela Aactualite 10 LT', price: '119,90', image: actualite },
+  { id: 8, name: 'Centrífuga Plus 3,9 LT', price: '119,90', image: centrifuga },
+  { id: 9, name: 'Café com Filtro 3,7 LT', price: '137,90', image: cafe },
+  { id: 10, name: '4 Tigelinha Alegra 250 ml', price: '79,60', image: tigela },
+  { id: 11, name: 'Cristalwave 1 lt', price: '84,90', image: cristalwave1 },
+  { id: 12, name: 'Cristalwave 3 lt', price: '89,90', image: cristalwave2 },
+  { id: 13, name: 'Descascador Tupperware', price: '34,90', image: descascador }
 ]
 
 export default function Product() {
@@ -113,7 +35,18 @@ export default function Product() {
 
   if (!id) return
 
-  const product = products.find((p) => p.id === parseInt(id)) // <-- converte para número
+  useEffect(() => {
+    // se a página foi aberta direto (sem passar pelo React Router),
+    // recarrega para forçar o carregamento correto
+    if (
+      !window.location.hash &&
+      !document.referrer.includes(window.location.host)
+    ) {
+      window.location.href = window.location.href
+    }
+  }, [])
+
+  const product = products.find((p) => p.id === parseInt(id))
 
   if (!product)
     return (
